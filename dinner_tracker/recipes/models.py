d9a7f226662251dfd5ea5ce03d5beaf40b55b3f2
Benjamin Lowry <Lowry_46@hotmail.com>
2022-11-12 18:18:08 -0500

    Added Recipe model and updated forms and urls

--- dinner_tracker/recipes/models.py
+++ dinner_tracker/recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.shortcuts import render, redirect, reverse
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    ONESTAR = "1."
+    TWOSTAR = "2."
+    THREESTAR = "3."
+    FOURSTAR = "4"
+    FIVESTAR = "5"
+    MY_CHOICES = [
+        (ONESTAR, "One Star"),
+        (TWOSTAR, "Two Star"),
+        (THREESTAR, "Three Star"),
+        (FOURSTAR, "Four Star"),
+        (FIVESTAR, "Five Star"),
+        ]
+    name = models.CharField(max_length=100)
+    ingredient = models.CharField(max_length=1000)
+    instructions = models.TextField(blank=True)
+    rating = models.CharField(max_length=10, choices=MY_CHOICES)
+
+    def get_absolute_url(self):
+        return reverse("create_recipe")
+
+
+
--- dinner_tracker/recipes/views.py
+++ dinner_tracker/recipes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import RecipeForm
+from .models import Recipe
 
 # Create your views here.
+
+
+def home(request):
+    context = {"name": "Dinner Tracker"}
+    return render(request, "recipes/home.html", context)
+
+
+class CreateRecipeView(CreateView):
+    model = Recipe
+    template_name = "recipes/recipe_create.html"
+    form_class = RecipeForm
